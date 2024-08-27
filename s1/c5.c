6331d486eb989eb5819cd9845f77ae13629ce9b9
anna <anna@brokenlove.online>
2024-08-27 23:14:00 +0200

    c5 done

---
+++ s1/c5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <ba.h>
+#include <cryptanalysis.h>
+
+int main()
+{
+	ba *a, *b;
+
+	a = ba_from_string("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal");
+	b = ba_from_string("ICE");
+	encrypt_rkxor(a, b);
+	ba_fprint(a, stdout, 0);
+	printf("\n");
+}
